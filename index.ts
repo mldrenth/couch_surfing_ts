b95b57cb2ff54f21dbf0f5331cedf3f0babcdfdf
Marijke Drenth <drenth.marijke@googlemail.com>
2022-07-22 21:00:38 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+// Number Types mini-challenge
+// Write a function that will only accept numbers and attend to 
+// all 'any' TypeScript weakness flags.
+const reviewTotalDisplay = document.querySelector('#reviews')
+
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+function showTotalReviews(value: number) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviews[0].name
+}
+
+
+showTotalReviews(reviews.length)
