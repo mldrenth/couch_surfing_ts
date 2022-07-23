ac31a6f04ce29c9fbd5767e70f723ea1147d22a5
Marijke Drenth <drenth.marijke@googlemail.com>
2022-07-24 00:04:47 +0300

    Finish primitive types

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-// Number Types mini-challenge
-// Write a function that will only accept numbers and attend to 
-// all 'any' TypeScript weakness flags.
+// Fixing the Website
+
+// Can you fix my code to show 'Welcome back Bobby' in the Nav Bar for 
+// our couch surfing website? Do this by assigning types to the parameters
+// we pass to our populateUser function, so that we can be aware of
+// errors in our user objects in the future.
+
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
+
 
 const reviews = [
     {
@@ -24,9 +31,29 @@ const reviews = [
     },
 ]
 
-function showTotalReviews(value: number) {
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviews[0].name
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+
+
+const you = {
+    userName: {firstName: 'Bobby', lastName: 'Brown'},
+    isReturning: true,
 }
 
 
-showTotalReviews(reviews.length)
+function populateUser(isReturning: boolean, userName: string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName.firstName)
+
